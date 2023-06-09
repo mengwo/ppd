a4088201f951883fa77860b5008a36e727d691e4
Emmanuel P. Larion <emmanuellarion14@gmail.com>
2023-06-09 13:30:23 +0800

    latest with 80% dashboard

---
+++ components/main/index.php
@@ -0,0 +1,24 @@
+<div class="main-header">
+    <div class="main-title">
+        <div class="ppd-title">
+            <h1>Port Police Department</h1>
+            <h4>PMO-NCN</h4>
+        </div>
+        <div class="date">
+            <input type="date" />
+        </div>
+    </div>
+    <div class="main-logo">
+        <img src="../../ppd/img/body-logo.png" alt="ppa logo">
+    </div>
+
+</div>
+<div class="main-content">
+    <section class="cases">
+        <h4>Previous case</h4>
+        <?php
+           include '..\ppd\components\cases\index.php';
+        ?>
+    </section>
+    <section class="summary">lorem</section>
+</div>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="./library/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="./css/base.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    <title>Port Police Department</title>
+
+</head>
+
+<body>
+    <div class="main-container">
+        <aside>
+            <?php
+            include './components/navigation/index.php';
+            ?>
+        </aside>
+        <main>
+            <?php
+            include './components/main/index.php';
+            ?>
+        </main>
+    </div>
+    <script src="./library/js/bootstrap.min.js"></script>
+</body>
+
+</html>
